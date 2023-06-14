3528441a81ec4533dd87b679ed0ca6037dd5c8b4
hanoys <hanoys@yandex.ru>
2023-06-14 22:37:46 +0300

    format code style

--- internal/app/messenger.go
+++ internal/app/messenger.go
@@ -16,31 +16,31 @@ import (
 )
 
 func Run(configPath string) {
-	config, err := config.GetConfig(configPath)
-    if err != nil {
-        log.Fatalf("cannot load config: %v", err)
-    }
+	cfg, err := config.GetConfig(configPath)
+	if err != nil {
+		log.Fatalf("cannot load config: %v", err)
+	}
 
-	dbpool, err := postgres.CreateConnectionPool(context.Background(), config.DB.URL)
-    if err != nil {
+	dbpool, err := postgres.CreateConnectionPool(context.Background(), cfg.DB.URL)
+	if err != nil {
 		log.Fatalf("unable to establish connection with database: %v", err)
-    }
+	}
 
-    repo := repository.NewRepositories(dbpool)
+	repo := repository.NewRepositories(dbpool)
 	services := service.NewServices(repo)
-    redisClient := redis.NewRedisClient(config.Redis.Host, config.Redis.Port)
-    tokenProvider := auth.NewProvider(redisClient, config)
-	handler := handler.NewHandler(services, tokenProvider)
+	redisClient := redis.NewRedisClient(cfg.Redis.Host, cfg.Redis.Port)
+	tokenProvider := auth.NewProvider(redisClient, cfg)
+	serviceHandler := handler.NewHandler(services, tokenProvider)
 
 	server := http.Server{
-		Handler:      handler.Init(),
+		Handler:      serviceHandler.Init(),
 		Addr:         ":8080",
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 	}
 
-    log.Printf("Starting server at: %v\n", server.Addr)
-    if err := server.ListenAndServe(); err != nil {
-        log.Fatalf("error while listening: %v", err)
-    }
+	log.Printf("Starting server at: %v\n", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("error while listening: %v", err)
+	}
 }
